ab92b25d775a797ef8e6cbae0e704997360f2d57
FeVent11 <115628289+FeVent11@users.noreply.github.com>
2022-11-03 16:01:41 +0700

    Felix

---
+++ app/src/main/java/com/ukm/com.uts.2127005/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ukm.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    fun aloha (view: View) {
+         android:id="@+id/textView2"
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
